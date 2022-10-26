ee1f599173a7d58b015372bbbfdf85af111916be
Selvavigneshv <selvavigneshmech118@gmail.com>
2022-10-26 15:34:18 +0530

    MyTask

---
+++ src/org/tcs/Sample.java
@@ -0,0 +1,17 @@
+package org.tcs;
+
+public class Sample {
+	public void empId() {
+System.out.println("My Emp Name : 1234");
+	}
+	public void empName() {
+System.out.println("My Emp Name : selva");
+	}
+	public static void main(String[] args) {
+		Sample s = new Sample();
+		s.empId();
+		s.empName();
+		
+	}
+
+}
